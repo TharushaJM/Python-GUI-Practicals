64d16638d377eecbf4929754cb9962264f6eb2b9
TharushaGonawala <157585076+TharushaJM@users.noreply.github.com>
2025-10-24 16:05:09 +0530

    gui

---
+++ creatingGUI/createtkintergui.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+
+#crete a window
+root = tk.Tk()
+root.title("Hello World")
+#root.iconbitmap('icon.ico')
+root.geometry(f'{400}x{400}+100+100') #width,height
+root.resizable(False,False)
+
+#run the window
+root.mainloop() 
+       
+              
---
+++ creatingGUI/test.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+
+
+#crete a window
+root = tk.Tk()
+root.title("Hello World")
+root.geometry('400x400+100+100') #width,height
+root.resizable(False,False)
+
+
+
+
+
+
+
+
+#run the window
+root.mainloop() 
